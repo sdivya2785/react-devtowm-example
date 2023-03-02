802c77c59ebed72b3de0c8741c5cdcb42025e805
sdivya2785 <59965654+sdivya2785@users.noreply.github.com>
2023-03-02 11:11:03 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Heading from "./Heading";
+import Note from "./Note";
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
